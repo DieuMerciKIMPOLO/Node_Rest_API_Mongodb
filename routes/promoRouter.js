4f08671ec926ab14a6bb12170043ed08c8ab878f
Dieu Merci <dieumerci.k.nkokolo@aims-senegal.org>
2019-03-09 09:00:21 +0000

    update

---
+++ routes/promoRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the propmotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /propmotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all propmotions');
+});
+
+dishRouter.route('/:propmoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the propmotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /propmotions/:promoId');
+})
+.delete((req,res,next)=>{
+    res.end('Deleting dish: ' + req.params.propmoId);
+})
+
+module.exports = dishRouter;
